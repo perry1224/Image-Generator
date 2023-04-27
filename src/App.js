d4ea443e7718ef17b1637c9f02bb927436ab8fac
perry1224 <perry.w1224@gmail.com>
2023-04-27 13:20:19 -0400

    image generator

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import { useState } from "react"
+
+const App = () => {
+  const [images, setImages] = useState (null)
+  const [value, setValue] = useState(null)
+  const [error, setError] = useState(null)
+  const surpriseOptions = [
+    'A blue ostrich eating melon',
+    'Cartoon drawing of a green Tokyo tower',
+    'A pineapple sunbathing on an island'
+  ]
+
+  const surpriseMe = () => {
+    setImages(null)
+    const randomValue = surpriseOptions[Math.floor(Math.random()* surpriseOptions.length)]
+    setValue(randomValue)
+  }
+
+  const getImages = async () => {
+    setImages(null)
+    if (value ===null) {
+      setError('Error! Must have a search term')
+      return
+    }
+    try {
+        const options = {
+          method: "POST",
+          body: JSON.stringify({
+            message: value,
+          }),
+          headers: {
+            "Content-type": "application/json"
+          }
+        }
+      const response = await fetch ('http://localhost:8000/images', options)
+      const data = await response.json()
+      console.log(data)
+      setImages(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  console.log(value)
+  return (
+    <div className="App">
+      <section className="search-section">
+        <p>Start with a detailed description 
+        <span className="surprise" onClick={surpriseMe}>Surprise me</span>
+        </p>
+        <div className="input-container">
+          <input 
+          value = {value}
+          placeholder="Use your imagination"
+          onChange={e => setValue(e.target.value)}
+          />
+          <button onClick={getImages}>Generate</button>
+        </div>
+       {error && <p>{error}</p>}
+      </section>
+      <section className="image-section">
+        {images?.map((image, _index) => (
+          <img key={_index} src={image.url} alt={`Generated image of ${value}`}/>
+        ))}
+      </section>
+    </div>
+  )
+}
+
+export default App;
